7e002c01ba2356514b33b323628803ddd1d11cf7
gekonator <github@gekonator.com>
2026-08-25 13:23:46 +0000

    Changes in data, clearer CSV

--- data.py
+++ data.py
@@ -1,13 +1,6 @@
 import pandas as pd
 import numpy as np
 
-def load_aox_csv(path: str) -> pd.DataFrame:
-    return pd.read_csv(
-        path,
-        sep=';',
-        decimal=',',
-        thousands='\u00A0',
-    )
+solar_db = pd.read_csv('data/csv/solar.csv')
+print(solar_db.head(100))
 
-balance_delta_df = load_aox_csv('data/csv/balance_delta.csv')
-print(balance_delta_df.head(10))
